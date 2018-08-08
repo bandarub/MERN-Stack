7fe486649d698f64f0964c4440d70b59bb0764f0
bandarub <bandaru.sravanthi@gmail.com>
2018-08-08 09:44:25 +0300

    added routing in server

--- server.js
+++ server.js
@@ -23,7 +23,7 @@ mongoose
 app.use('/api/students',students);
 
 app.use(express.static('client/build'))
-app.get('/',(req, res) => res.sendFile(path.join(__dirname, 'client/build/index.html')));
+app.get('*',(req, res) => res.sendFile(path.join(__dirname, 'client/build/index.html')));
 
 //Define port
 const port = process.env.PORT||5000;
