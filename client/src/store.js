958cb352d52abb77cb19f5e424b2481b2bc83168
bandarub <bandaru.sravanthi@gmail.com>
2018-09-04 12:51:30 +0300

    added Procfile

--- client/src/store.js
+++ client/src/store.js
@@ -1,24 +1,17 @@
 import { createStore, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
 import combineReducer from './reducers/reducers';
-import {composeWithDevTools} from 'redux-devtools-extension/developmentOnly';
-import {createLogger} from 'redux-logger';
-
-const logger = createLogger({
-    /* https://github.com/evgenyrodionov/redux-logger */
-    collapsed: true,
-    diff: true
-});
-
 
 const initialState = {};
 
+const middleware = [thunk];
 
 const store = createStore(
   combineReducer,
   initialState,
   compose(
-    composeWithDevTools(applyMiddleware(thunk, logger))  
+    applyMiddleware(...middleware),
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
   )
 );
 
