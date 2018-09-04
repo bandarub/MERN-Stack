076f1af2b891fe1b0ab838bfb12e0ae6e14cfc09
bandarub <bandaru.sravanthi@gmail.com>
2018-09-04 12:57:54 +0300

    added Procfile

--- client/src/store.js
+++ client/src/store.js
@@ -7,9 +7,6 @@ const initialState = {};
 const middleware = [thunk];
 let devTools = window.__REDUX_DEVTOOLS_EXTENSION__ && 
 window.__REDUX_DEVTOOLS_EXTENSION__();
-if (process.env.NODE_ENV === 'prod' || process.env.NODE_ENV === 'production') {
-    devTools = a => a;
-}
 
 const store = createStore(
   combineReducer,
