0effffe1b16c0bff83b3e1cc0853f65ff2769bb9
bandarub <bandaru.sravanthi@gmail.com>
2019-02-20 17:28:21 +0200

    added feature

--- client/src/Components/appnavbar.js
+++ client/src/Components/appnavbar.js
@@ -10,36 +10,40 @@ import {
   Container
 } from "reactstrap";
 
-class AppNavbar extends Component {
-  state = {
-    isOpen: false
-  };
-
-  toggle = () => {
-    this.setState({ isOpen: !this.state.isOpen });
-  };
-  render() {
-    return (
-      <div>
-        <Navbar color="dark" dark expand="sm" className="mb-5">
-          <Container>
-            <NavbarBrand href="/">Integrify Students List</NavbarBrand>
-            <NavbarToggler onClick={this.toggle} />
-            <Collapse isOpen={this.state.isOpen} navbar>
-              <Nav className="ml-auto" navbar>
-                <NavLink
-                  target="_blank"
-                  href="https://github.com/bandarub/MERN-Stack"
-                >
-                  Github
-                </NavLink>
-              </Nav>
-            </Collapse>
-          </Container>
-        </Navbar>
-      </div>
-    );
+  class AppNavbar extends Component{
+      
+        state = {
+            isOpen :false
+         }
+ 
+      toggle = ()=>{
+          this.setState({isOpen:!this.state.isOpen})
+      }
+      render(){
+          return(
+            <div>
+            <Navbar color="dark" dark expand="sm" className="mb-5">
+                <Container>
+                    <NavbarBrand href='/'>
+                    <img className='logo' src='https://res.cloudinary.com/arysri/image/upload/v1533620944/integrifylogo.jpg'/>
+                        Students List
+                    </NavbarBrand>
+                    <NavbarToggler onClick={this.toggle} />
+                    <Collapse isOpen={this.state.isOpen} navbar>
+                        <Nav className="ml-auto" navbar>
+                            <NavItem>
+                            <NavLink href="https://github.com/bandarub/MERN-Stack">
+                                Github
+                            </NavLink>
+                            </NavItem>
+                        </Nav>
+                    </Collapse>
+                </Container>
+            </Navbar>
+            </div>
+          )
+      }
   }
 }
 
-export default AppNavbar;
+  export default AppNavbar;
